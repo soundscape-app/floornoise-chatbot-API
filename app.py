e5d1a95b3944d799e087d42cb2bd47e94b8d439e
HAIL <s000715@naver.com>
2022-07-26 14:17:30 +0900

    220726: add analysis, l-index algorithm modify

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from matplotlib import text
 import nltk
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from sqlalchemy import true
 from sound import load_audio
 from numpy.typing import *
 from sound.level import calculate_rms, rms2db, calculate_lamax, calculate_laeq, find_spike_indices, lamax_adjustment
@@ -168,61 +169,40 @@ def update_threshold():
     return analysis_info['spike_indices'].shape[0]
 
 def update_noise_level(user_sensitivity, user_lamax):
+    noise_level = 6
+    t_low = (40, 43, 50, 58, 65, 76)
+    t_normal = (40, 42.5, 48, 54, 60, 73)
+    t_high = (39, 41, 47, 53.5, 58.5, 68)
+    
     if user_sensitivity == 'low':
-        if user_lamax <= 40:
-            return 0
-        elif user_lamax <= 43:
-            return 1
-        elif user_lamax <= 50:
-            return 2
-        elif user_lamax <= 58:
-            return 3
-        elif user_lamax <= 65:
-            return 4
-        elif user_lamax <= 76:
-            return 5
-        else:
-            return 6
+        for i in range(len(t_low)):
+            if user_lamax <= t_low[i]:
+                noise_level = i
     elif user_sensitivity == 'high':
-        if user_lamax <= 39:
-            return 0
-        elif user_lamax <= 41:
-            return 1
-        elif user_lamax <= 47:
-            return 2
-        elif user_lamax <= 53.5:
-            return 3
-        elif user_lamax <= 58.5:
-            return 4
-        elif user_lamax <= 68:
-            return 5
-        else:
-            return 6
-    else: #user_sensitivity == 'normal
-        if user_lamax <= 40:
-            return 0
-        elif user_lamax <= 42.5:
-            return 1
-        elif user_lamax <= 48:
-            return 2
-        elif user_lamax <= 54:
-            return 3
-        elif user_lamax <= 60:
-            return 4
-        elif user_lamax <= 73:
-            return 5
-        else:
-            return 6        
+        for i in range(len(t_high)):
+            if user_lamax <= t_high[i]:
+                noise_level = i
+    else: # user_sensitivity == 'normal'
+        for i in range(len(t_normal)):
+            if user_lamax <= t_normal[i]:
+                noise_level = i
+    
+    return noise_level
     
 def update_lindex(filename):
     wav, sr = librosa.load(filename)
     wav, sr = A_weighting(wav, sr)
     ref = 2 * 10 ** (-5)
 
-    f63 = signal.firwin(101, cutoff=[62, 64], fs=sr, pass_zero='bandpass')
-    f125 = signal.firwin(101, cutoff=[122, 124], fs=sr, pass_zero='bandpass')
-    f250 = signal.firwin(101, cutoff=[249, 251], fs=sr, pass_zero='bandpass')
-    f500 = signal.firwin(101, cutoff=[499, 501], fs=sr, pass_zero='bandpass')
+    f63 = signal.firwin(11, cutoff=[62, 64], fs=sr, pass_zero='bandpass')
+    f125 = signal.firwin(11, cutoff=[122, 124], fs=sr, pass_zero='bandpass')
+    f250 = signal.firwin(11, cutoff=[249, 251], fs=sr, pass_zero='bandpass')
+    f500 = signal.firwin(11, cutoff=[499, 501], fs=sr, pass_zero='bandpass')
+    
+    # band63 = f63
+    # band125 = f125
+    # band250 = f250
+    # band500 = f500
     
     band63 = signal.lfilter(f63, [1,0], wav)
     band125 = signal.lfilter(f125, [1,0], wav)
@@ -251,32 +231,58 @@ def update_lindex(filename):
         np.max(spl500).item()
     ]
     
-    if band_lamax[0]>110 or band_lamax[1]>100 or band_lamax[2]>93 or band_lamax[3]>87:
-        l_index = 85
-    elif band_lamax[0]>105 or band_lamax[1]>95 or band_lamax[2]>88 or band_lamax[3]>82:
-        l_index = 80
-    elif band_lamax[0]>100 or band_lamax[1]>90 or band_lamax[2]>83 or band_lamax[3]>77:
-        l_index = 75
-    elif band_lamax[0]>95 or band_lamax[1]>85 or band_lamax[2]>78 or band_lamax[3]>72:
-        l_index = 70
-    elif band_lamax[0]>90 or band_lamax[1]>80 or band_lamax[2]>73 or band_lamax[3]>67:
-        l_index = 65
-    elif band_lamax[0]>85 or band_lamax[1]>75 or band_lamax[2]>68 or band_lamax[3]>62:
-        l_index = 60
-    elif band_lamax[0]>80 or band_lamax[1]>70 or band_lamax[2]>63 or band_lamax[3]>57:
-        l_index = 55
-    elif band_lamax[0]>75 or band_lamax[1]>65 or band_lamax[2]>58 or band_lamax[3]>52:
-        l_index = 50
-    elif band_lamax[0]>70 or band_lamax[1]>60 or band_lamax[2]>53 or band_lamax[3]>47:
-        l_index = 45
-    elif band_lamax[0]>65 or band_lamax[1]>55 or band_lamax[2]>48 or band_lamax[3]>42:
-        l_index = 40
-    elif band_lamax[0]>60 or band_lamax[1]>50 or band_lamax[2]>43 or band_lamax[3]>37:
-        l_index = 35
-    else:
-        l_index = 30
+    print(band_lamax)
+    
+    l_index = 85
+    t63 = tuple(range(110, 54, -5))
+    t125 = tuple(range(100, 44, -5))
+    t250 = tuple(range(93, 37, -5))
+    t500 = tuple(range(87, 31, -5))
+    t_index = tuple(range(85, 29, -5))
+    
+    for i in range(len(t_index)):
+        if band_lamax[0] > t63[i] or band_lamax[1] > t125[i] or band_lamax[2] > t250[i] or band_lamax[3] > t500[i]:
+            l_index = t_index[i]
+            break
         
     return l_index
+
+def analysis_sentence(user_noise_level, user_l_index):
+    t_noise_level_sentences = [
+        '실내 환경은 조용합니다. 외부 소음에 노출되지 않았습니다.',
+        '소음이 당신을 괴롭히지 않습니다. 소음이 일상생활에 지장을 줄 만큼 크지 않습니다.',
+        '인내심을 가지고 소음 문제에 대처할 수 있는 수준입니다. 소음이 지속되어 괴롭다면, 이웃과 소통하시기를 권장합니다.',
+        '소음 문제에 대처하기 위해 이웃과 소통할 필요가 있습니다. 주의를 부탁드려 보세요. 이웃에게 측정 결과를 제시하고 이야기를 나눠보세요.',
+        '소음이 당신의 휴식 시간을 방해합니다. 층간소음 법적 기준을 초과합니다. 이웃과 적극적으로 소통할 필요가 있습니다. 만약 소음이 계속 발생한다면 이웃사이 센터에 상담을 문의하세요.',
+        '소음으로 인해 거주 공간에서 대화가 들리지 않습니다. 소음으로부터 견디기 어려운 상황입니다. 층간소음 법적 기준을 상당히 초과합니다. 이웃과 소통에 어려움이 있다면 이웃사이 센터에 상담을 문의하세요.',
+        '사람이 이곳에 사는 것은 불가능합니다.'
+    ]
+    t_l_index_sentences = [
+        ['뛰는 행위나 발걸음 소리가 거의 들리지 않습니다.\n', '의자나 물건이 떨어지는 소리가 전혀 들리지 않습니다.\n', '어린이가 크게 소리쳐도 괜찮습니다.\n'],
+        ['뛰는 행위나 발걸음 소리가 조용할 때 들립니다.\n', '의자나 물건이 떨어지는 소리가 들리지 않습니다.\n', '다소 뛰어 다녀도 괜찮습니다.\n'],
+        ['뛰는 행위나 발걸음 소리가 멀리서 들리는 느낌입니다.\n', '의자나 물건이 떨어지는 소리가 거의 들리지 않습니다.\n', '소음을 신경쓰지 않고 생활할 수 있습니다.\n'],
+        ['뛰는 행위나 발걸음 소리가 들리지만 거슬리지 않습니다.\n', '샌들을 신고 걷는 소리가 들립니다.\n', '소음의 존재를 약간 알 수 있습니다.\n'],
+        ['뛰는 행위나 발걸음 소리가 들리지만 거의 거슬리지 않습니다.\n', '칼을 사용하는 소리가 들립니다.\n', '소음을 약간 주의하면서 생활할 수 있습니다.\n'],
+        ['뛰는 행위나 발걸음 소리에 약간의 마음이 쓰입니다.\n', '슬리퍼를 신고 걷는 소리가 들립니다.\n', '소음을 주의한다면 문제 없을 것입니다.\n'],
+        ['뛰는 행위나 발걸음 소리가 약간 거슬립니다.\n', '수저를 떨어뜨리는 소리가 들립니다.\n', '소음이 상호 간에 견딜 수 있는 정도입니다.\n'],
+        ['뛰는 행위나 발걸음 소리가 잘 들려 거슬립니다.\n', '동전을 떨어뜨리는 소리가 들립니다.\n', '소음을 발생시킨 어린이를 꾸짖게 됩니다.\n'],
+        ['뛰는 행위나 발걸음 소리가 매우 잘 들려 거슬립니다.\n', '1원짜리 동전을 떨어뜨리는 소리가 들립니다.\n', '어른들도 마음을 쓰게 되는 수준입니다.\n'],
+        ['뛰는 행위나 발걸음 소리가 매우 귀찮습니다.\n', '주의하여도 시비가 붙는 수준입니다.\n'],
+        ['뛰는 행위나 발걸음 소리가 시끄러워 견딜 수 없습니다.\n', '소음을 참는 생활이 필요한 수준입니다.\n']
+    ]
+    
+    l_index = list(range(30, 86, 5))
+    
+    sentence = ''
+    sentence += '녹음된 전체 음원을 통해 주거 환경의 소음 발생 정도를 분석합니다. \n'
+    for i in range(len(l_index)):
+        if l_index[i] == user_l_index:
+            for j in t_l_index_sentences[i]:
+                sentence += j
+    sentence += '발생한 최고 소음을 기반으로 층간소음의 정도를 분석합니다. \n'
+    sentence += t_noise_level_sentences[user_noise_level]
+    
+    return sentence
 
 @app.route('/')
 def setparameter():
@@ -289,7 +295,7 @@ def setparameter():
     return "Parameters are set"
 
 @app.route('/showresult')
-def showparameter():
+def showresult():
     return jsonify({
         "laeq": str(user_laeq),
         "lamax": str(user_lamax),
@@ -298,6 +304,15 @@ def showparameter():
         "noise_level": str(user_noise_level),
         "l-Index" : str(user_l_index)
     })
+    
+@app.route('/analysis')
+def analysis():
+    return analysis_sentence(user_noise_level, user_l_index)
+    
+
+@app.route('/brief-analysis')
+def brief():
+    return
  
 if __name__ == "__main__":
     app.run()
